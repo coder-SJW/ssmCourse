ecdeab6b673e36cf238cb82670120555087579ad
apple <2855289441@qq.com>
2020-11-03 11:31:33 +0800

    日常更新：简单类型和bean的依赖注入

--- src/main/java/com/ssmStudy/pojo/People.java
+++ src/main/java/com/ssmStudy/pojo/People.java
@@ -1,10 +1,23 @@
 package com.ssmStudy.pojo;
 
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
 /**
- * used for spring test
+ * used for spring test,用于熟悉控制反转、依赖注入
  */
 public class People {
     private Rice rice;
+    private Rice[] riceArr;
+    private List<String> list;
+    private Map<String,String> map;
+    private Set<String> set;
+    private List<Rice> listForBean;
+    private Map<String,Rice> mapForBean;
+    private Set<Rice> setForBean;
+    private Map<String,String> mapPro;
+
     public  People(){};
     public People(Rice rice) {
         this.rice = rice;
@@ -13,12 +26,75 @@ public class People {
     public Rice getRice() {
         return rice;
     }
-
     public void setRice(Rice rice) {
         this.rice = rice;
     }
-
     public void eat(){
+        System.out.println(list.size());
         rice.showName();
+    }
+
+    public List<String> getList() {
+        return list;
+    }
+
+    public void setList(List<String> list) {
+        this.list = list;
+    }
+
+    public Map<String, String> getMap() {
+        return map;
+    }
+
+    public void setMap(Map<String, String> map) {
+        this.map = map;
+    }
+
+    public Set<String> getSet() {
+        return set;
+    }
+
+    public void setSet(Set<String> set) {
+        this.set = set;
+    }
+
+    public Rice[] getRiceArr() {
+        return riceArr;
+    }
+
+    public void setRiceArr(Rice[] riceArr) {
+        this.riceArr = riceArr;
+    }
+
+    public List<Rice> getListForBean() {
+        return listForBean;
+    }
+
+    public void setListForBean(List<Rice> listForBean) {
+        this.listForBean = listForBean;
+    }
+
+    public Map<String, Rice> getMapForBean() {
+        return mapForBean;
+    }
+
+    public void setMapForBean(Map<String, Rice> mapForBean) {
+        this.mapForBean = mapForBean;
+    }
+
+    public Set<Rice> getSetForBean() {
+        return setForBean;
+    }
+
+    public void setSetForBean(Set<Rice> setForBean) {
+        this.setForBean = setForBean;
+    }
+
+    public Map<String, String> getMapPro() {
+        return mapPro;
+    }
+
+    public void setMapPro(Map<String, String> mapPro) {
+        this.mapPro = mapPro;
     }
 }
--- src/test/java/SpringTest.java
+++ src/test/java/SpringTest.java
@@ -8,6 +8,17 @@ public class SpringTest {
     public void startSpring(){
         ApplicationContext ac = new ClassPathXmlApplicationContext("applicationContext.xml");
         People people = (People) ac.getBean("people");
-        people.eat();
+        System.out.println("简单类型输入---》");
+        System.out.println("list里注入简单类型："+people.getList().get(0));;
+        System.out.println("map里注入简单类型："+people.getMap().get("a"));
+        System.out.println("set里注入简单类型,大小："+people.getSet().size());
+        System.out.println("map里注入简单类型："+people.getMapPro().get("a"));;
+        System.out.println("bean注入---》");
+        System.out.println("list里注入bean:"+people.getListForBean().get(0));
+        System.out.println("map里注入bean："+people.getMapForBean().get("a"));
+        System.out.println("set里注入bean,大小："+people.getSetForBean().size());
+        System.out.println("数组里注入bean,大小："+people.getRiceArr().length);
+
     }
+
 }
